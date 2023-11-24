2b690a2c9bd94b7318de5b49929bf611cc2ece66
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-11-24 15:08:33 +0100

    Add EF Core dependency

---
+++ ExpenseSplitter.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseSplitter.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
