9501c5b09c3acc8a8672aef5c318ac4cd6edb341
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-11-26 20:37:07 +0100

    Implement dependency injection tests

---
+++ ExpenseSplitter.Application.UnitTests/DependencyInjectionTests.cs
@@ -0,0 +1,32 @@
+﻿using ExpenseSplitter.Application.Abstractions.Behaviors;
+using ExpenseSplitter.Application.Settlements.CreateSettlement;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpenseSplitter.Application.UnitTests;
+
+public class DependencyInjectionTests
+{
+    [Fact]
+    public void AddApplication_ShouldAddMediatR()
+    {
+        var services = new ServiceCollection();
+
+        var result = DependencyInjection.AddApplication(services);
+
+        services.Should().Contain(x => x.ServiceType == typeof(IPublisher));
+        services.Should().Contain(x => x.ImplementationType == typeof(ValidationBehavior<,>));
+        services.Should().Contain(x => x.ImplementationType == typeof(LoggingBehavior<,>));
+    }
+
+    [Fact]
+    public void AddApplication_ShouldAddValidators()
+    {
+        var services = new ServiceCollection();
+
+        DependencyInjection.AddApplication(services);
+
+        services.Should().Contain(x => x.ServiceType == typeof(IValidator<CreateSettlementCommand>));
+    }
+}
