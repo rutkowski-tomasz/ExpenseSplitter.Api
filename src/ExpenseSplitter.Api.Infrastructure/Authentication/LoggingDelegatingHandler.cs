c27fbeeb197a77aa9923d4f986eaef9081781aae
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-01-15 12:44:10 +0100

    feat: create and register logging delegating handler

---
+++ src/ExpenseSplitter.Api.Infrastructure/Authentication/LoggingDelegatingHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ExpenseSplitter.Api.Infrastructure.Authentication
+{
+    public class LoggingDelegatingHandler : DelegatingHandler
+    {
+        private readonly ILogger<LoggingDelegatingHandler> logger;
+
+        public LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger)
+        {
+            this.logger = logger;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                logger.LogTrace(
+                    "Sending HTTP request to {Method} {Uri} with {Content}",
+                    request.Method,
+                    request.RequestUri,
+                    request.Content
+                );
+
+                var result = await base.SendAsync(request, cancellationToken);
+
+                logger.LogInformation("After HTTP request");
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "HTTP request failed");
+
+                throw;
+            }
+        }
+    }
+}
