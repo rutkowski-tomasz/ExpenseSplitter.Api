cd40c0333dfd99daf4582ab445065ba4e4823848
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-01-20 15:01:44 +0100

    test: settlement join and leave

---
+++ test/ExpenseSplitter.Api.Application.UnitTests/Settlements/JoinSettlementCommandHandlerTests.cs
@@ -0,0 +1,95 @@
+using ExpenseSplitter.Api.Application.Abstractions.Authentication;
+using ExpenseSplitter.Api.Application.Settlements.CreateSettlement;
+using ExpenseSplitter.Api.Application.Settlements.JoinSettlement;
+using ExpenseSplitter.Api.Domain.Abstractions;
+using ExpenseSplitter.Api.Domain.Settlements;
+using ExpenseSplitter.Api.Domain.SettlementUsers;
+
+namespace ExpenseSplitter.Api.Application.UnitTests.Settlements;
+
+public class JoinSettlementCommandHandlerTests
+{
+    private readonly Fixture fixture;
+    private readonly Mock<ISettlementRepository> settlementRepositoryMock;
+    private readonly Mock<ISettlementUserRepository> settlementUserRepositoryMock;
+    private readonly JoinSettlementCommandHandler handler;
+
+    public JoinSettlementCommandHandlerTests()
+    {
+        fixture = CustomFixutre.Create();
+        settlementRepositoryMock = new Mock<ISettlementRepository>();
+        settlementUserRepositoryMock = new Mock<ISettlementUserRepository>();
+        var userContextMock = new Mock<IUserContext>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        settlementRepositoryMock
+            .Setup(x => x.GetByInviteCode(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(fixture.Create<Settlement>());
+
+        settlementUserRepositoryMock
+            .Setup(x => x.CanUserAccessSettlement(It.IsAny<SettlementId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(false);
+
+        handler = new JoinSettlementCommandHandler(
+            settlementRepositoryMock.Object,
+            settlementUserRepositoryMock.Object,
+            userContextMock.Object,
+            unitOfWorkMock.Object
+        );
+    }
+
+    [Fact]
+    public async Task Validate_ShouldFail_WhenInviteCodeIsEmpty()
+    {
+        var command = fixture
+            .Build<JoinSettlementCommand>()
+            .With(x => x.InviteCode, string.Empty)
+            .Create();
+
+        var validator = new JoinSettlementCommandValidator();
+        var result = await validator.ValidateAsync(command);
+
+        result.Errors.Should().Contain(x => x.PropertyName == nameof(command.InviteCode));
+    }
+
+    [Fact]
+    public async Task Handle_ShouldFail_WhenSettlementWithInviteCodeDoesNotExist()
+    {
+        settlementRepositoryMock
+            .Setup(x => x.GetByInviteCode(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Settlement) null);
+
+        var command = fixture.Create<JoinSettlementCommand>();
+
+        var response = await handler.Handle(command, default);
+
+        response.IsFailure.Should().BeTrue();
+        response.Error.Code.Should().Be(SettlementErrors.NotFound.Code);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldFail_WhenUserAlreadyJoinedSettlement()
+    {
+        settlementUserRepositoryMock
+            .Setup(x => x.CanUserAccessSettlement(It.IsAny<SettlementId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var command = fixture.Create<JoinSettlementCommand>();
+
+        var response = await handler.Handle(command, default);
+
+        response.IsFailure.Should().BeTrue();
+        response.Error.Code.Should().Be(SettlementUserErrors.AlreadyJoined.Code);
+    }
+
+    [Fact]
+    public async Task Handle_ShoulSuccess()
+    {
+        var command = fixture.Create<JoinSettlementCommand>();
+
+        var response = await handler.Handle(command, default);
+
+        response.IsSuccess.Should().BeTrue();
+        response.Value.Should().NotBeEmpty();
+    }
+}
---
+++ test/ExpenseSplitter.Api.Application.UnitTests/Settlements/LeaveSettlementCommandHandlerTests.cs
@@ -0,0 +1,68 @@
+using ExpenseSplitter.Api.Application.Settlements.LeaveSettlement;
+using ExpenseSplitter.Api.Domain.Abstractions;
+using ExpenseSplitter.Api.Domain.Settlements;
+using ExpenseSplitter.Api.Domain.SettlementUsers;
+
+namespace ExpenseSplitter.Api.Application.UnitTests.Settlements;
+
+public class LeaveSettlementCommandHandlerTests
+{
+    private readonly Fixture fixture;
+    private readonly Mock<ISettlementUserRepository> settlementUserRepositoryMock;
+    private readonly LeaveSettlementCommandHandler handler;
+
+    public LeaveSettlementCommandHandlerTests()
+    {
+        fixture = CustomFixutre.Create();
+        settlementUserRepositoryMock = new Mock<ISettlementUserRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        settlementUserRepositoryMock
+            .Setup(x => x.GetBySettlementId(It.IsAny<SettlementId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(fixture.Create<SettlementUser>());
+
+        handler = new LeaveSettlementCommandHandler(
+            settlementUserRepositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+    }
+
+    [Fact]
+    public async Task Validate_ShouldFail_WhenSettlementIdIsEmpty()
+    {
+        var command = fixture
+            .Build<LeaveSettlementCommand>()
+            .With(x => x.SettlemetId, Guid.Empty)
+            .Create();
+
+        var validator = new LeaveSettlementCommandValidator();
+        var result = await validator.ValidateAsync(command);
+
+        result.Errors.Should().Contain(x => x.PropertyName == nameof(command.SettlemetId));
+    }
+
+    [Fact]
+    public async Task Handle_ShouldFail_WhenSettlementWithInviteCodeDoesNotExist()
+    {
+        settlementUserRepositoryMock
+            .Setup(x => x.GetBySettlementId(It.IsAny<SettlementId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((SettlementUser) null);
+
+        var command = fixture.Create<LeaveSettlementCommand>();
+
+        var response = await handler.Handle(command, default);
+
+        response.IsFailure.Should().BeTrue();
+        response.Error.Code.Should().Be(SettlementErrors.Forbidden.Code);
+    }
+
+    [Fact]
+    public async Task Handle_ShoulSuccess()
+    {
+        var command = fixture.Create<LeaveSettlementCommand>();
+
+        var response = await handler.Handle(command, default);
+
+        response.IsSuccess.Should().BeTrue();
+    }
+}
