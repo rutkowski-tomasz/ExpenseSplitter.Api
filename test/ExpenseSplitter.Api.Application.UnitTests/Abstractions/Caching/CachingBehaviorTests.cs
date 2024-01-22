a14790bb901aa2a3cd89f09ddc1634edd11eeb60
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-01-22 10:46:17 +0100

    test: caching and idempotency behavior

---
+++ test/ExpenseSplitter.Api.Application.UnitTests/Abstractions/Caching/CachingBehaviorTests.cs
@@ -0,0 +1,44 @@
+using ExpenseSplitter.Api.Application.Abstractions.Caching;
+using ExpenseSplitter.Api.Application.Abstractions.Cqrs;
+using ExpenseSplitter.Api.Domain.Abstractions;
+using MediatR;
+
+namespace ExpenseSplitter.Api.Application.UnitTests.Abstractions.Caching;
+
+public class CachingBehaviorTests
+{
+    private readonly Mock<ICacheService> cacheService;
+    private readonly CachingBehavior<TestQuery, Result<int>> behavior;
+
+    public CachingBehaviorTests()
+    {
+        cacheService = new Mock<ICacheService>();
+        behavior = new CachingBehavior<TestQuery, Result<int>>(cacheService.Object);
+    }
+
+    private record TestQuery : ICachedQuery
+    {
+        public string Key => "test";
+        public TimeSpan? Expiration => null;
+    };
+
+    [Fact]
+    public async Task Handle_ShouldUseCachedResult_WhenPresent()
+    {
+        var cachedResult = Result.Success(2137);
+        cacheService
+            .Setup(x => x.GetOrCreateAsync(
+                It.Is<string>(x => x == "test"),
+                It.IsAny<Func<CancellationToken, Task<Result<int>>>>(),
+                It.IsAny<TimeSpan?>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(cachedResult);
+
+        var next = new RequestHandlerDelegate<Result<int>>(() => Task.FromResult(Result.Success(1)));
+        var result = await behavior.Handle(new TestQuery(), next, default);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(2137);
+    }
+}
---
+++ test/ExpenseSplitter.Api.Application.UnitTests/Abstractions/Idempotency/IdempotentBehaviorTests.cs
@@ -0,0 +1,94 @@
+using ExpenseSplitter.Api.Application.Abstractions.Cqrs;
+using ExpenseSplitter.Api.Application.Abstractions.Idempotency;
+using ExpenseSplitter.Api.Domain.Abstractions;
+using MediatR;
+
+namespace ExpenseSplitter.Api.Application.UnitTests.Abstractions.Idempotency;
+
+public class IdempotentBehaviorTests
+{
+    private readonly Mock<IIdempotencyService> idempotencyService;
+    private readonly IdempotentBehavior<TestCommand, Result<int>> behavior;
+    private Guid idempotencyKey;
+
+    public IdempotentBehaviorTests()
+    {
+        idempotencyService = new Mock<IIdempotencyService>();
+
+        idempotencyService
+            .Setup(x => x.IsIdempotencyKeyInHeaders())
+            .Returns(true);
+
+        idempotencyKey = Guid.NewGuid();
+        idempotencyService
+            .Setup(x => x.TryParseIdempotencyKey(out idempotencyKey))
+            .Returns(true);
+
+        behavior = new IdempotentBehavior<TestCommand, Result<int>>(
+            idempotencyService.Object
+        );
+    }
+
+    private class TestCommand : IBaseCommand
+    {
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnNextValue_WhenIdempotencyKeyIsNotHeaders()
+    {
+        idempotencyService
+            .Setup(x => x.IsIdempotencyKeyInHeaders())
+            .Returns(false);
+
+        var next = new RequestHandlerDelegate<Result<int>>(() => Task.FromResult(Result.Success(3)));
+
+        var result = await behavior.Handle(new TestCommand(), next, default);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(3);
+    }
+    
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenIdempotencyKeyIsNotParseable()
+    {
+        Guid parsedIdempotencyKey;
+        idempotencyService
+            .Setup(x => x.TryParseIdempotencyKey(out parsedIdempotencyKey))
+            .Returns(false);
+
+        var next = new RequestHandlerDelegate<Result<int>>(() => Task.FromResult(Result.Success(43)));
+
+        var result = await behavior.Handle(new TestCommand(), next, default);
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Code.Should().Be("Idempotency.IdempotentKeyNotGuid");
+    }
+    
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenIdempotencyKeyWasProcessed()
+    {
+        idempotencyService
+            .Setup(x => x.IsIdempotencyKeyProcessed(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var next = new RequestHandlerDelegate<Result<int>>(() => Task.FromResult(Result.Success(43)));
+
+        var result = await behavior.Handle(new TestCommand(), next, default);
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Code.Should().Be("Idempotency.IdempotentKeyAlreadyProcessed");
+    }
+    
+    [Fact]
+    public async Task Handle_ShouldSuccess()
+    {
+        var next = new RequestHandlerDelegate<Result<int>>(() => Task.FromResult(Result.Success(43)));
+
+        var result = await behavior.Handle(new TestCommand(), next, default);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(43);
+        
+        idempotencyService.Verify(x => x.SaveIdempotencyKey(idempotencyKey, "TestCommand", It.IsAny<CancellationToken>()));
+    }
+}
