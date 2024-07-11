39798d03029724b5c778aa5e177ba1fb5d990e9e
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-07-11 09:38:55 +0200

    refactor: handler refactoring

--- src/ExpenseSplitter.Api.Presentation/Abstractions/Handler.cs
+++ src/ExpenseSplitter.Api.Presentation/Abstractions/Handler.cs
@@ -21,100 +21,56 @@ public interface IHandlerEmptyResponse<TRequest, TCommand>
     Task<IResult> Handle(TRequest request);
 }
 
-public class Handler<TRequest, TCommand, TCommandResult, TResponse>(
-    IMapper<TRequest, TCommand> requestMapper,
-    ISender sender,
-    IMapper<TCommandResult, TResponse> responseMapper,
-    IHttpContextAccessor httpContextAccessor
-) : IHandler<TRequest, TCommand, TCommandResult, TResponse>
-    where TCommand : IRequest<Result<TCommandResult>>
+public abstract class BaseHandler(
+    IHttpContextAccessor httpContextAccessor,
+    ISender sender
+)
 {
-    public async Task<IResult> Handle(TRequest request)
-    {
-        var command = requestMapper.Map(request);
+    protected Task<Result<TCommandResult>> HandleCommand<TCommand, TCommandResult>()
+        where TCommand : IRequest<Result<TCommandResult>>, new()
+        => SendCommand<TCommand, Result<TCommandResult>>(new TCommand());
 
+    protected Task<Result> HandleCommand<TRequest, TCommand>(TRequest request, IMapper<TRequest, TCommand> requestMapper)
+        where TCommand : IRequest<Result>
+        => SendCommand<TCommand, Result>(requestMapper.Map(request));
+
+    protected Task<Result<TCommandResult>> HandleCommand<TRequest, TCommand, TCommandResult>(TRequest request, IMapper<TRequest, TCommand> requestMapper)
+        where TCommand : IRequest<Result<TCommandResult>>
+        => SendCommand<TCommand, Result<TCommandResult>>(requestMapper.Map(request));
+
+    private async Task<TCommandResult> SendCommand<TCommand, TCommandResult>(TCommand command)
+        where TCommand : IRequest<TCommandResult>
+    {
         var cancellationToken = httpContextAccessor.HttpContext?.RequestAborted ?? CancellationToken.None;
         var result = await sender.Send(command, cancellationToken);
-
-        if (result.IsSuccess)
-        {
-            var response = responseMapper.Map(result.Value);
-            return TypedResults.Ok(response);
-        }
-
-        var detail = result.Error.Description;
-        return result.Error.Type switch
-        {
-            ErrorType.Validation => Results.BadRequest(detail),
-            ErrorType.NotFound => Results.NotFound(detail),
-            ErrorType.Forbidden => Results.Forbid(),
-            ErrorType.BadRequest => Results.BadRequest(detail),
-            ErrorType.PreConditionFailed => Results.Problem(detail, statusCode: StatusCodes.Status412PreconditionFailed),
-            ErrorType.Conflict => Results.Conflict(detail),
-            ErrorType.BadGateway => Results.Problem(detail, statusCode: StatusCodes.Status502BadGateway),
-            ErrorType.Unauthorized => Results.Unauthorized(),
-            ErrorType.NotModified => Results.StatusCode(StatusCodes.Status304NotModified),
-            _ => Results.Problem(detail, statusCode: StatusCodes.Status500InternalServerError)
-        };
+        return result;
     }
-}
 
-public class HandlerEmptyRequest<TCommand, TCommandResult, TResponse>(
-    ISender sender,
-    IMapper<TCommandResult, TResponse> responseMapper,
-    IHttpContextAccessor httpContextAccessor
-) : IHandlerEmptyRequest<TCommand, TCommandResult, TResponse>
-    where TCommand : IRequest<Result<TCommandResult>>, new()
-{
-    public async Task<IResult> Handle()
+    protected IResult HandleResult(Result result)
     {
-        var cancellationToken = httpContextAccessor.HttpContext?.RequestAborted ?? CancellationToken.None;
-        var result = await sender.Send(new TCommand(), cancellationToken);
-
-        if (result.IsSuccess)
-        {
-            var response = responseMapper.Map(result.Value);
-            return TypedResults.Ok(response);
-        }
-
-        var detail = result.Error.Description;
-        return result.Error.Type switch
-        {
-            ErrorType.Validation => Results.BadRequest(detail),
-            ErrorType.NotFound => Results.NotFound(detail),
-            ErrorType.Forbidden => Results.Forbid(),
-            ErrorType.BadRequest => Results.BadRequest(detail),
-            ErrorType.PreConditionFailed => Results.Problem(detail, statusCode: StatusCodes.Status412PreconditionFailed),
-            ErrorType.Conflict => Results.Conflict(detail),
-            ErrorType.BadGateway => Results.Problem(detail, statusCode: StatusCodes.Status502BadGateway),
-            ErrorType.Unauthorized => Results.Unauthorized(),
-            ErrorType.NotModified => Results.StatusCode(StatusCodes.Status304NotModified),
-            _ => Results.Problem(detail, statusCode: StatusCodes.Status500InternalServerError)
-        };
-    }
-}
-
-public class HandlerEmptyResponse<TRequest, TCommand>(
-    IMapper<TRequest, TCommand> requestMapper,
-    ISender sender,
-    IHttpContextAccessor httpContextAccessor
-) : IHandlerEmptyResponse<TRequest, TCommand>
-    where TCommand : IRequest<Result>
-{
-    public async Task<IResult> Handle(TRequest request)
-    {
-        var command = requestMapper.Map(request);
-
-        var cancellationToken = httpContextAccessor.HttpContext?.RequestAborted ?? CancellationToken.None;
-        var result = await sender.Send(command, cancellationToken);
-
         if (result.IsSuccess)
         {
             return TypedResults.Ok();
         }
 
-        var detail = result.Error.Description;
-        return result.Error.Type switch
+        return HandleError(result.Error);
+    }
+
+    protected IResult HandleResult<TResult, TResponse>(Result<TResult> result, IMapper<TResult, TResponse> responseMapper)
+    {
+        if (result.IsSuccess)
+        {
+            var response = responseMapper.Map(result.Value);
+            return TypedResults.Ok(response);
+        }
+
+        return HandleError(result.Error);
+    }
+
+    protected IResult HandleError(Error error)
+    {
+        var detail = error.Description;
+        return error.Type switch
         {
             ErrorType.Validation => Results.BadRequest(detail),
             ErrorType.NotFound => Results.NotFound(detail),
@@ -128,4 +84,38 @@ public class HandlerEmptyResponse<TRequest, TCommand>(
             _ => Results.Problem(detail, statusCode: StatusCodes.Status500InternalServerError)
         };
     }
+}
+
+public class Handler<TRequest, TCommand, TCommandResult, TResponse>(
+    IHttpContextAccessor httpContextAccessor,
+    ISender sender,
+    IMapper<TRequest, TCommand> requestMapper,
+    IMapper<TCommandResult, TResponse> responseMapper
+) : BaseHandler(httpContextAccessor, sender), IHandler<TRequest, TCommand, TCommandResult, TResponse>
+where TCommand : IRequest<Result<TCommandResult>>
+{
+    public async Task<IResult> Handle(TRequest request)
+        => HandleResult(await HandleCommand<TRequest, TCommand, TCommandResult>(request, requestMapper), responseMapper);
+}
+
+public class HandlerEmptyResponse<TRequest, TCommand>(
+    IHttpContextAccessor httpContextAccessor,
+    ISender sender,
+    IMapper<TRequest, TCommand> requestMapper
+) : BaseHandler(httpContextAccessor, sender), IHandlerEmptyResponse<TRequest, TCommand>
+where TCommand : IRequest<Result>
+{
+    public async Task<IResult> Handle(TRequest request)
+        => HandleResult(await HandleCommand(request, requestMapper));
+}
+
+public class HandlerEmptyRequest<TCommand, TCommandResult, TResponse>(
+    IHttpContextAccessor httpContextAccessor,
+    ISender sender,
+    IMapper<TCommandResult, TResponse> responseMapper
+) : BaseHandler(httpContextAccessor, sender), IHandlerEmptyRequest<TCommand, TCommandResult, TResponse>
+where TCommand : IRequest<Result<TCommandResult>>, new()
+{
+    public async Task<IResult> Handle()
+        => HandleResult(await HandleCommand<TCommand, TCommandResult>(), responseMapper);
 }
--- src/ExpenseSplitter.Api.Presentation/Abstractions/IEndpoint.cs
+++ src/ExpenseSplitter.Api.Presentation/Abstractions/IEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace ExpenseSplitter.Api.Presentation.Abstractions;
+﻿using ExpenseSplitter.Api.Domain.Abstractions;
+using MediatR;
+
+namespace ExpenseSplitter.Api.Presentation.Abstractions;
 
 public interface IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder builder);
+}
+
+
+public interface IEndpoint<TRequest, TCommand> : IMapper<TRequest, TCommand>
+    where TCommand : IRequest<Result>
+{
+    public void MapEndpoint(IEndpointRouteBuilder builder);
+}
+
+public interface IEndpoint<TRequest, TCommand, TCommandResult, TResponse>
+    : IMapper<TRequest, TCommand>, IMapper<TCommandResult, TResponse>
+    where TCommand : IRequest<Result<TCommandResult>>
 {
     public void MapEndpoint(IEndpointRouteBuilder builder);
 }
