0c0032fb233d0cd2f6b81741fca2829bfd034970
Tomasz Rutkowski <11985426+rutkowski-tomasz@users.noreply.github.com>
2024-12-07 16:31:28 +0100

    feat: migrate to net8 update styling (#63)
    * chore: migrate to net8.0
    
    * chore: further migration to net8.0
    
    * refactor: use roslyn suggestions
    
    * fix: unit tests
    
    * fix: release build tests
    
    * refactor: change domain events to on save events
    
    * refactor: change error to AppError to avoid using reserved keyword
    
    * refactor: DomainEvents
    
    * feat: make Dockerfile generic, respect .editorconfig
    
    * fix: tests
    
    * fix: use arg only where used

---
+++ src/ExpenseSplitter.Api.Infrastructure/Idempotency/IdempotentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseSplitter.Api.Infrastructure.Idempotency;
+
+internal sealed class IdempotentRequest
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public DateTime CreatedOnUtc { get; init; }
+}
+
