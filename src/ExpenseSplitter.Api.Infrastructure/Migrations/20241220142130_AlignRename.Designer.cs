b0151df1b63b0c7615344bce27204bb0271ad28e
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-12-20 15:22:00 +0100

    feat: align database schema

--- src/ExpenseSplitter.Api.Infrastructure/Migrations/20241220142130_AlignRename.Designer.cs
+++ src/ExpenseSplitter.Api.Infrastructure/Migrations/20241220142130_AlignRename.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExpenseSplitter.Api.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExpenseSplitter.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241220142130_AlignRename")]
+    partial class AlignRename
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
